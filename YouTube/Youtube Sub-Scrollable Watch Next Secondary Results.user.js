36d905de808d4311df02b9609d91c625ff1501f3
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-03-04 15:54:28 -0800

    Changed file name

--- YouTube/Youtube Sub-Scrollable Watch Next Secondary Results.user.js
+++ YouTube/Youtube Sub-Scrollable Watch Next Secondary Results.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Sub-Scrollable "Watch Next Secondary Results"
-// @namespace    https://thealiendrew.github.io/
+// @namespace    https://github.com/TheAlienDrew/Tampermonkey-Scripts
 // @version      0.1
 // @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/YouTube/Youtube%20Sub-Scrollable%20_Watch%20Next%20Secondary%20Results_.user.js
 // @description  Converts the side video suggestions into a confined scrollable list, so you can watch your video while looking at suggestions.
