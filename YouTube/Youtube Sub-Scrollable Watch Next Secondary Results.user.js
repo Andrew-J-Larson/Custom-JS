cb3ba3336c843065492f60e9d8f7cb06d6784dd1
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-03-06 08:45:27 -0800

    Auto play video is now absolute positioned

--- YouTube/Youtube Sub-Scrollable Watch Next Secondary Results.user.js
+++ YouTube/Youtube Sub-Scrollable Watch Next Secondary Results.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Sub-Scrollable "Watch Next Secondary Results"
 // @namespace    https://github.com/TheAlienDrew/Tampermonkey-Scripts
-// @version      2.3
+// @version      2.4
 // @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/YouTube/Youtube%20Sub-Scrollable%20Watch%20Next%20Secondary%20Results.user.js
 // @description  Converts the side video suggestions into a confined scrollable list, so you can watch your video while looking at suggestions.
 // @author       AlienDrew
@@ -13,13 +13,18 @@
 const containerSelector = '#primary';
 const videoSelector = 'video';
 const watchNextSelector = 'ytd-watch-next-secondary-results-renderer';
+const autoPlaySelector = 'ytd-compact-autoplay-renderer';
+const itemsSelector = '#items .ytd-watch-next-secondary-results-renderer:nth-child(2)';
+const scrollbarWidth = 17;
 const timeForTimeouts = 1000;
 
 var $ = window.jQuery;
 var d = document;
 
 var visibility = d.visibilityState;
+var panelWidth = 0;
 var panelHeight = 0;
+var autoVidHeight = 0;
 
 // don't try to resize page isn't visible
 d.addEventListener('visibilitychange', function() {
@@ -39,7 +44,7 @@ function addStyleString(str) {
 }
 
 // detect position changes to change size accordingly
-function fixWatchNextSize() {
+function fixDynamicSizes() {
     if (visibility == 'visible') {
         var scrHeight  = $(window).height(),
             vidHeight  = $(videoSelector).height(),
@@ -54,14 +59,29 @@ function fixWatchNextSize() {
             addStyleString(watchNextSelector + ' { height: ' + viewHeight + 'vh;}');
         }
 
-        setTimeout(fixWatchNextSize, timeForTimeouts);
+        var watchNextWidth = $(watchNextSelector).outerWidth(),
+            autoPlayHeight = 0;
+        if ($(autoPlaySelector) != null) autoPlayHeight = $(autoPlaySelector).outerHeight(true);
+
+        if (panelWidth != watchNextWidth) {
+            panelWidth = watchNextWidth;
+            console.log(scrollbarWidth);
+            addStyleString(autoPlaySelector + ' { width: ' + (watchNextWidth - scrollbarWidth) + 'px; }');
+        }
+
+        if (autoVidHeight != autoPlayHeight) {
+            autoVidHeight = autoPlayHeight;
+            addStyleString(itemsSelector + ' { padding-top: ' + autoPlayHeight + 'px; }');
+        }
+
+        setTimeout(fixDynamicSizes, timeForTimeouts);
     }
 }
 
 // wait until watch next panel is given a position to start sizing
 function waitForPanelPosition(time) {
     if ($(watchNextSelector).position() != null) {
-        fixWatchNextSize()
+        fixDynamicSizes()
         return;
     } else {
         setTimeout(function() {
@@ -70,38 +90,40 @@ function waitForPanelPosition(time) {
     }
 }
 
-// prevent page from scolling when using panel scrolling
-// code via https://stackoverflow.com/a/33672757/7312536
-$(watchNextSelector).on('DOMMouseScroll mousewheel', function(ev) {
-    var $this = $(this),
-        scrollTop = this.scrollTop,
-        scrollHeight = this.scrollHeight,
-        height = $this.height(),
-        delta = (ev.type == 'DOMMouseScroll' ?
-                 ev.originalEvent.detail * -40 :
-                 ev.originalEvent.wheelDelta),
-        up = delta > 0;
-
-    var prevent = function() {
-        ev.stopPropagation();
-        ev.preventDefault();
-        ev.returnValue = false;
-        return false;
-    }
-
-    if (!up && -delta > scrollHeight - height - scrollTop) {
-        // Scrolling down, but this will take us past the bottom.
-        $this.scrollTop(scrollHeight);
-        return prevent();
-    } else if (up && delta > scrollTop) {
-        // Scrolling up, but this will take us past the top.
-        $this.scrollTop(0);
-        return prevent();
-    }
-});
-
 // enabled scrollbar on watch next panel, and start sizing
-waitForKeyElements(watchNextSelector, function () {
-    addStyleString(watchNextSelector + ' { overflow-y: auto !important; }');
+waitForKeyElements(itemsSelector, function () {
+    // prevent page from scolling when using panel scrolling
+    // code via https://stackoverflow.com/a/33672757/7312536
+    $(watchNextSelector).on('DOMMouseScroll mousewheel', function(ev) {
+        var $this = $(this),
+            scrollTop = this.scrollTop,
+            scrollHeight = this.scrollHeight,
+            height = $this.height(),
+            delta = (ev.type == 'DOMMouseScroll' ?
+                     ev.originalEvent.detail * -40 :
+                     ev.originalEvent.wheelDelta),
+            up = delta > 0;
+
+        var prevent = function() {
+            ev.stopPropagation();
+            ev.preventDefault();
+            ev.returnValue = false;
+            return false;
+        }
+
+        if (!up && -delta > scrollHeight - height - scrollTop) {
+            // Scrolling down, but this will take us past the bottom.
+            $this.scrollTop(scrollHeight);
+            return prevent();
+        } else if (up && delta > scrollTop) {
+            // Scrolling up, but this will take us past the top.
+            $this.scrollTop(0);
+            return prevent();
+        }
+    });
+
+    addStyleString(watchNextSelector + ' { overflow-y: scroll !important; }');
+    addStyleString(autoPlaySelector + ' { position: absolute; z-index: 100; background-color: rgba(0,0,0,0.9); }');
+
     waitForPanelPosition(100);
 });
