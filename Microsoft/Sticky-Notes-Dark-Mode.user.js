54c8c8ce02e0ca0ba114071c1974bbef20888e99
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-04-24 05:48:29 -0700

    Almost forgot the main code

--- Microsoft/Sticky-Notes-Dark-Mode.user.js
+++ Microsoft/Sticky-Notes-Dark-Mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Microsoft Sticky Notes - Dark Mode
 // @namespace    https://github.com/TheAlienDrew/Tampermonkey-Scripts
-// @version      1.1
+// @version      1.2
 // @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/Microsoft/Sticky-Notes-Dark-Mode.user.js
 // @description  Enables official, but hidden, dark mode on the Sticky Notes website.
 // @author       AlienDrew
@@ -11,14 +11,44 @@
 // ==/UserScript==
 
 // constants
+const fastDelay = 100;
 const urlDoubleQuote = '%22';
 const darkModeThemeId = '4';
 const darkModeLinkColor = urlDoubleQuote + '93CFF7' + urlDoubleQuote;
-const fastDelay = 100;
+const helpIFrameId = '#helpPaneFull iframe';
+// listener variables
+var helpIFrameLoaded = false;
+
 // function for elements
 function elementExists(element) {
     return (typeof(element) != 'undefined' && element != null);
 }
 
+// theme help iframe
+function checkForHelp() {
+    setTimeout(function() {
+        var helpIFrame = document.querySelector(helpIFrameId);
+        var helpIFrameExists = elementExists(helpIFrame);
+        if (helpIFrameExists && !helpIFrameLoaded) {
+
+            // get locations in string for theme and link edits
+            var oldURL = helpIFrame.src;
+            var themeIdStart = oldURL.indexOf(':', oldURL.indexOf('ThemeId')) + 1;
+            var themeIdEnd = oldURL.indexOf(',', themeIdStart);
+            var linkColorStart = oldURL.indexOf(':', oldURL.indexOf('LinkColor')) + 1;
+            var linkColorEnd = oldURL.indexOf(',', linkColorStart);
+
+            // create the new url
+            var newURL = oldURL.substring(0, themeIdStart) + darkModeThemeId + oldURL.substring(themeIdEnd, linkColorStart) + darkModeLinkColor + oldURL.substring(linkColorEnd);
+
+            // change to the new URL
+            helpIFrame.src = newURL;
+            helpIFrameLoaded = true;
+        } else if (!helpIFrameExists) helpIFrameLoaded = false;
+        checkForHelp();
+    }, fastDelay);
+}
+
 // apply the dark mode class to the html element
 document.body.classList.add('n-darkMode');
+checkForHelp();
