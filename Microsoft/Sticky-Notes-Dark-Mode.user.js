e3834114907db25e8a6f9cc1bb49525461c925a8
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-04-24 05:37:24 -0700

    Help pane no longer a sore thumb
    It now matches the Sticky notes dark theme instead of being super black.

--- Microsoft/Sticky-Notes-Dark-Mode.user.js
+++ Microsoft/Sticky-Notes-Dark-Mode.user.js
@@ -1,14 +1,24 @@
 // ==UserScript==
 // @name         Microsoft Sticky Notes - Dark Mode
 // @namespace    https://github.com/TheAlienDrew/Tampermonkey-Scripts
-// @version      1.0
+// @version      1.1
 // @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/Microsoft/Sticky-Notes-Dark-Mode.user.js
 // @description  Enables official, but hidden, dark mode on the Sticky Notes website.
 // @author       AlienDrew
 // @match        *://www.onenote.com/stickynotes*
-// @run-at       document-start
+// @run-at       document-body
 // @noframes
 // ==/UserScript==
 
+// constants
+const urlDoubleQuote = '%22';
+const darkModeThemeId = '4';
+const darkModeLinkColor = urlDoubleQuote + '93CFF7' + urlDoubleQuote;
+const fastDelay = 100;
+// function for elements
+function elementExists(element) {
+    return (typeof(element) != 'undefined' && element != null);
+}
+
 // apply the dark mode class to the html element
-document.documentElement.classList.add('n-darkMode');
+document.body.classList.add('n-darkMode');
