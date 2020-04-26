b89fed5561af3175e3c04c81ef8ae06a8b840102
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-04-26 05:38:40 -0700

    Changed link color

--- Microsoft/Sticky-Notes-Dark-Mode.user.js
+++ Microsoft/Sticky-Notes-Dark-Mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Microsoft Sticky Notes - Dark Mode
 // @namespace    https://github.com/TheAlienDrew/Tampermonkey-Scripts
-// @version      2.1
+// @version      2.2
 // @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/Microsoft/Sticky-Notes-Dark-Mode.user.js
 // @description  Enables official, but hidden, dark mode on the Sticky Notes website.
 // @author       AlienDrew
@@ -48,7 +48,7 @@ var injectCss = function(documentToInject, cssStyle) {
 if (currentURL.startsWith(stickyNotesWebsite)) {// code to run on the sticky notes website
     const urlDoubleQuote = '%22';
     const darkModeThemeId = '4';
-    const darkModeLinkColor = urlDoubleQuote + '93CFF7' + urlDoubleQuote;
+    const darkModeLinkColor = urlDoubleQuote + 'b3d6fc' + urlDoubleQuote;
     const helpIFrameId = '#helpPaneFull iframe';
     // listener variables
     var helpIFrameLoaded = false;
