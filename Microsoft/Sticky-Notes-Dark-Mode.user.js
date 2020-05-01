641a301a6f00ba887567828a86700df7f726ca8e
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-04-30 20:11:38 -0700

    Updating version
    The theme was changed, and updating the version on here is the only way for it to recache the css file on everyone's computer.

--- Microsoft/Sticky-Notes-Dark-Mode.user.js
+++ Microsoft/Sticky-Notes-Dark-Mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Microsoft Sticky Notes - Dark Mode
 // @namespace    https://github.com/TheAlienDrew/Tampermonkey-Scripts
-// @version      2.3
+// @version      2.4
 // @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/Microsoft/Sticky-Notes-Dark-Mode.user.js
 // @description  Enables official, but hidden, dark mode on the Sticky Notes website.
 // @author       AlienDrew
