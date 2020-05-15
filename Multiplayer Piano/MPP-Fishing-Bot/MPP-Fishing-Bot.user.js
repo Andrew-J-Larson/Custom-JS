02e568cb4a15ba4243c27ee210c80a493352b4b9
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-05-14 23:11:02 -0700

    Almost for got to change this

--- Multiplayer Piano/MPP-Fishing-Bot/MPP-Fishing-Bot.user.js
+++ Multiplayer Piano/MPP-Fishing-Bot/MPP-Fishing-Bot.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Fishing Bot
 // @namespace    https://thealiendrew.github.io/
-// @version      1.2.0
-// @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/Multiplayer%20Piano/MPP-Fishing-Bot.user.js
+// @version      1.2.1
+// @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/Multiplayer%20Piano/MPP-Fishing-Bot/MPP-Fishing-Bot.user.js
 // @description  Fishes for new colors!
 // @author       AlienDrew
 // @include      /^https?://www\.multiplayerpiano\.com/test/fishing$/
