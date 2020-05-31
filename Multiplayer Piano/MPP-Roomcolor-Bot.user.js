2e23b2996d4bd3f1a2ef96969e48ef34129e9c0f
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-05-31 01:54:34 -0700

    Changes room color command info into its own bot

--- Multiplayer Piano/MPP-Roomcolor-Bot.user.js
+++ Multiplayer Piano/MPP-Roomcolor-Bot.user.js
@@ -1,4 +1,54 @@
+// ==UserScript==
+// @name         Roomcolor Bot
+// @namespace    https://thealiendrew.github.io/
+// @version      0.1.0
+// @description  Advanced roomcolor changing!
+// @author       AlienDrew
+// @include      /^https?://www\.multiplayerpiano\.com*/
+// @downloadURL  https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Multiplayer%20Piano/MPP-Roomcolor-Bot.user.js
+// @icon         https://icons.iconarchive.com/icons/streamlineicons/streamline-ux-free/512/color-palette-icon.png
+// @grant        GM_info
+// @run-at       document-end
+// @noframes
+// ==/UserScript==
+
+/* Copyright (C) 2020  Andrew Larson (thealiendrew@gmail.com)
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* globals MPP */
+
 // =============================================== CONSTANTS
+
+// Script constants
+const SCRIPT = GM_info.script;
+const NAME = SCRIPT.name;
+const NAMESPACE = SCRIPT.namespace;
+const VERSION = SCRIPT.version;
+const DESCRIPTION = SCRIPT.description;
+const AUTHOR = SCRIPT.author;
+const DOWNLOAD_URL = SCRIPT.downloadURL;
+
+// Time constants (in milliseconds)
+const TENTH_OF_SECOND = 100; // mainly for repeating loops
+const SECOND = 10 * TENTH_OF_SECOND;
+const CHAT_DELAY = 5 * TENTH_OF_SECOND; // needed since the chat is limited to 10 messages within less delay
+const SLOW_CHAT_DELAY = 2 * SECOND // when you are not the owner, your chat quota is lowered
+
+// URLs
+const FEEDBACK_URL = "https://forms.gle/TMWH55k1TChwNGLp9";
 
 // MPP Constants (these are not meant to be changed); roomcolor arrays: [0] = inner, [1] = outer
 const MPP_DEFAULT_ROOMCOLORS = ["rgb(59, 80, 84)", "rgb(0, 16, 20)"];
@@ -9,9 +59,20 @@ const INNER_ROOM_COLOR = 0; // used in room color settings (DON'T CHANGE)
 const OUTER_ROOM_COLOR = 1; // used in room color settings (DON'T CHANGE)
 
 // Bot constant settings
-const PREFIX = "/";
+const PREFIX = "!";
+const PREFIX_LENGTH = PREFIX.length;
+const BOT_USERNAME = NAME + " [" + PREFIX + "help]";
+const BOT_NAMESPACE = '(' + NAMESPACE + ')';
+const BOT_DESCRIPTION = DESCRIPTION + " Made with JS via Tampermonkey."
+const BOT_AUTHOR = "Created by " + AUTHOR + '.';
 const BOT_ROOM_COLORS = ["#000000", "#046307"]; // these are the colors the bot will set the room to by default
-const ROOM_OWNER_COMMANDS = [
+const BASE_COMMANDS = [
+    ["help (command)", "displays info about command, but no command entered shows the commands"],
+    ["about", "get information about this bot"],
+    ["link", "get the download link for this bot"],
+    ["feedback", "shows link to send feedback about the bot to the developer"]
+];
+const BOT_COMMANDS = [
     ["roomcolor (command)", "displays info about room color command, but no command shows the room color commands and special color options"]
 ];
 const ROOMCOLOR_OPTIONS = "Options: normal [bot set room color(s)], default [the MPP general room color(s)], lobby [the MPP lobby room color(s)], but entering nothing shows the current color(s)";
@@ -21,6 +82,21 @@ const ROOMCOLOR_COMMANDS = [
     ["roomcolors (option/color)", "sets both the inner and outer room colors (one color)"],
     ["roomcolors ([color1] [color2])", "sets both the inner and outer room colors (separate colors)"]
 ];
+const PRE_MSG = NAME + " (v" + VERSION + "): ";
+const PRE_HELP = PRE_MSG + "[Help]";
+const PRE_ABOUT = PRE_MSG + "[About]";
+const PRE_LINK = PRE_MSG + "[Link]";
+const PRE_FEEDBACK = PRE_MSG + "[Feedback]";
+const PRE_ROOMCOLOR = PRE_MSG + "[Roomcolor]";
+const PRE_ERROR = PRE_MSG + "Error!";
+const NOT_OWNER = "The bot isn't the owner of the room";
+const LIST_BULLET = "• ";
+const DESCRIPTION_SEPARATOR = " - ";
+
+// =============================================== VARIABLES
+
+var active = true; // turn off the bot if needed
+var chatDelay = CHAT_DELAY; // for how long to wait until posting another message
 
 // =============================================== FUNCTIONS
 
@@ -28,6 +104,41 @@ const ROOMCOLOR_COMMANDS = [
 var exists = function(element) {
     if (typeof(element) != "undefined" && element != null) return true;
     return false;
+}
+
+// Puts quotes around string
+var quoteString = function(string) {
+    var newString = string;
+    if (exists(string) && string != "") newString = '"' + string + '"';
+    return newString
+}
+
+// Makes all commands into one string
+var formattedCommands = function(commandsArray, prefix, spacing) { // needs to be 2D array with commands before descriptions
+    if (!exists(prefix)) prefix = '';
+    var commands = '';
+    var i;
+    for(i = 0; i < commandsArray.length; ++i) {
+        commands += (spacing ? ' ' : '') + prefix + commandsArray[i][0];
+    }
+    return commands;
+}
+
+// Gets 1 command and info about it into a string
+var formatCommandInfo = function(commandsArray, commandIndex) {
+    return LIST_BULLET + PREFIX + commandsArray[commandIndex][0] + DESCRIPTION_SEPARATOR + commandsArray[commandIndex][1];
+}
+
+// Send messages without worrying about timing
+var mppChatSend = function(str, delay) {
+    setTimeout(function(){MPP.chat.send(str)}, (exists(delay) ? delay : 0));
+}
+
+// When there is an incorrect command, show this error
+var cmdNotFound = function(cmd) {
+    var error = PRE_ERROR + " Invalid command, " + quoteString(cmd) + " doesn't exist";
+    if (active) mppChatSend(error);
+    else console.log(error);
 }
 
 // Validates colors
@@ -287,6 +398,48 @@ var mppRoomColorSend = function(area, color, delay) { // area is the INNER or OU
 }
 
 // Commands
+var help = function(command, userId, yourId) {
+    var isOwner = MPP.client.isOwner();
+    if (!exists(command) || command == "") {
+        var publicCommands = formattedCommands(BOT_COMMANDS, LIST_BULLET + PREFIX, true);
+        mppChatSend(PRE_HELP + " Commands: " + formattedCommands(BASE_COMMANDS, LIST_BULLET + PREFIX, true)
+                             + publicCommands);
+    } else {
+        var valid = null;
+        var commandIndex = null;
+        var commandArray = null;
+        command = command.toLowerCase();
+        // check commands arrays
+        var i;
+        for(i = 0; i < BASE_COMMANDS.length; i++) {
+            if (BASE_COMMANDS[i][0].indexOf(command) == 0) {
+                valid = command;
+                commandArray = BASE_COMMANDS;
+                commandIndex = i;
+            }
+        }
+        var j;
+        for(j = 0; j < BOT_COMMANDS.length; j++) {
+            if (BOT_COMMANDS[j][0].indexOf(command) == 0) {
+                valid = command;
+                commandArray = BOT_COMMANDS;
+                commandIndex = j;
+            }
+        }
+        // display info on command if it exists
+        if (exists(valid)) mppChatSend(PRE_HELP + ' ' + formatCommandInfo(commandArray, commandIndex),);
+        else cmdNotFound(command);
+    }
+}
+var about = function() {
+    mppChatSend(PRE_ABOUT + ' ' + BOT_DESCRIPTION + ' ' + BOT_AUTHOR + ' ' + BOT_NAMESPACE);
+}
+var link = function() {
+    mppChatSend(PRE_LINK + " You can download this bot from " + DOWNLOAD_URL);
+}
+var feedback = function() {
+    mppChatSend(PRE_FEEDBACK + " Please go to " + FEEDBACK_URL + " in order to submit feedback.");
+}
 var roomcolor = function(command) {
     if (!exists(command) || command == "") {
         mppChatSend(PRE_ROOMCOLOR + ' ' + ROOMCOLOR_OPTIONS, 0);
@@ -350,8 +503,15 @@ var roomcolors = function(argsColors) {
 // =============================================== MAIN
 
 MPP.client.on('a', function (msg) {
+    // if user switches to VPN, these need to update
+    var yourParticipant = MPP.client.getOwnParticipant();
+    var yourId = yourParticipant._id;
+    var yourUsername = yourParticipant.name;
     // get the message as string
     var input = msg.a.trim();
+    var participant = msg.p;
+    var username = participant.name;
+    var userId = participant._id;
 
     // make sure the start of the input matches prefix
     if (input.startsWith(PREFIX)) {
@@ -363,6 +523,10 @@ MPP.client.on('a', function (msg) {
         var arguments = (hasArgs != -1) ? argumentsString.split(' ') : null;
         // look through commands
         switch (command.toLowerCase()) {
+            case "help": case "h": if (active) help(argumentsString, userId, yourId); break;
+            case "about": case "ab": if (active) about(); break;
+            case "link": case "li": if (active) link(); break;
+            case "feedback": case "fb": if (active) feedback(); break;
             case "roomcolor": case "rc": if (active) roomcolor(argumentsString); break;
             case "roomcolor1": case "rc1": if (active) roomcolor1(argumentsString); break;
             case "roomcolor2": case "rc2": if (active) roomcolor2(argumentsString); break;
@@ -370,3 +534,27 @@ MPP.client.on('a', function (msg) {
         }
     }
 });
+MPP.client.on("ch", function(msg) {
+    // set new chat delay based on room ownership after changing rooms
+    if (!MPP.client.isOwner()) chatDelay = SLOW_CHAT_DELAY;
+    else chatDelay = CHAT_DELAY;
+});
+
+// =============================================== INTERVALS
+
+// Automatically turns off the sound warning (mainly for autoplay)
+var clearSoundWarning = setInterval(function() {
+    var playButton = document.querySelector("#sound-warning button");
+    if (exists(playButton)) {
+        clearInterval(clearSoundWarning);
+        playButton.click();
+        // wait for the client to come online
+        var waitForMPP = setInterval(function() {
+            if (exists(MPP) && exists(MPP.client) && exists(MPP.client.channel) && exists(MPP.client.channel._id) && MPP.client.channel._id != "") {
+                clearInterval(waitForMPP);
+
+                console.log(PRE_MSG + " Online!");
+            }
+        }, TENTH_OF_SECOND);
+    }
+}, TENTH_OF_SECOND);
