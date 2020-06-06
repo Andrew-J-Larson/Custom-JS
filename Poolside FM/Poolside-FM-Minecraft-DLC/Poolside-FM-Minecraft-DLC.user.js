4ead287518480361d1c2d090ddf71d57a8dca8f4
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-06-06 08:12:58 -0700

    Delay lengthened again + better resize use + fullscreen button now

--- Poolside FM/Poolside-FM-Minecraft-DLC/Poolside-FM-Minecraft-DLC.user.js
+++ Poolside FM/Poolside-FM-Minecraft-DLC/Poolside-FM-Minecraft-DLC.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Poolside FM - Minecraft DLC
 // @namespace    https://thealiendrew.github.io/
-// @version      0.2.1
+// @version      0.3.0
 // @description  Allows toggling the video to a playable version of Minecraft Classic!
 // @author       AlienDrew
 // @match        https://poolside.fm/*
@@ -31,44 +31,90 @@
 
 const TENTH_OF_SECOND = 100;
 const SECOND = 10 * TENTH_OF_SECOND;
-const UPDATE_GUI_TIMEOUT = 7.5 * TENTH_OF_SECOND;
+const UPDATE_GUI_TIMEOUT = 2.5*SECOND;
 const YT_LOADER_TIMEOUT = SECOND;
 
 // =============================================== APP CONSTANTS (NOT TO CHANGE)
 
-const APP_ID = "widget2";
+const IFRAME_ID = "widget2";
 const HIDE_GRAPHIC_SELECTOR = ".hide-graphic";
 const INNER_WRAPPER_SELECTOR = ".inner-wrapper";
-const HANDLE_BR_SELECTOR = ".handle-br";
 const YT_LOADER_SELECTOR = ".youtube-loader";
+const DESKTOP_ID = "app";
+const TASKBAR_SELECTOR = '#' + DESKTOP_ID + " > div > header";
+const RESTORE_DOWN_CLASS = "restore-down";
+const MAXIMIZE_CLASS = "maximize";
+const MINIMIZE_CLASS = "minimize";
+const HIDE_DESKTOP_CLASS = "hide-desktop";
 
 // =============================================== APP CONSTANTS (feel free to change)
 
 const APP_NAME = "Minecraft";
-const APP_WINDOW_ID = APP_NAME.toLowerCase();
+const APP_ID = APP_NAME.toLowerCase();
 const APP_WEBSITE = "https://classic.minecraft.net/";
-const APP_FAVICON = "https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Poolside%20FM/Poolside-FM-Minecraft-DLC/favicons/retro_dithered.png";
-const APP_SPLASH = "https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Poolside%20FM/Poolside-FM-Minecraft-DLC/logos/mojang.png";
+const APP_RAW_LINK = "https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Poolside%20FM/Poolside-FM-Minecraft-DLC/";
+const APP_ICONS_LINK = APP_RAW_LINK + "icons/";
+const APP_FAVICON = APP_RAW_LINK + "favicons/retro_dithered.png";
+const APP_SPLASH = APP_RAW_LINK + "logos/mojang.png";
+const MAXIMIZE_ICON = APP_ICONS_LINK + "maximize.png";
+const RESTORE_DOWN_ICON = APP_ICONS_LINK + "restore-down.png";
+const RELOAD_ICON = APP_ICONS_LINK + "reload.png";
+const TASKBAR_ID = "taskbar";
+const SIZER_ID = "sizer-iframe";
+const RELOAD_ID = "reload-iframe";
 const APP_SPLASH_ID = "app-splash";
-const RELOAD_ICON = "https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Poolside%20FM/Poolside-FM-Minecraft-DLC/icons/reload.png";
-const SANDBOX_IFRAME = "allow-same-origin allow-scripts allow-pointer-lock"; // disabled: allow-popups allow-forms
+const SANDBOX_IFRAME = "allow-same-origin allow-scripts allow-pointer-lock allow-forms allow-modals allow-popups";
 const STYLE_APP_SPLASH = "image-rendering: pixelated; image-rendering: -moz-crisp-edges; image-rendering: crisp-edges;";
-const STYLE_LOAD_IFRAME = "left: 100vw; top: 100vh; pointer-events: none; width: 200%; height: 200%;"; // must be an absurd size to reize back to normal
-const STYLE_IFRAME = "width: 100%; height: 100%; transform: none; left: auto; top: auto; border: 1px solid #000; -appkit-box-shadow: inset 1px 1px 0 0 #fff, 5px 5px 0 rgba(0,0,0,.2); box-shadow: inset 1px 1px 0 0 #fff, 5px 5px 0 rgba(0,0,0,.2);";
-const STYLE_HIDE_GRAPHIC_INNER_WRAPPER = "padding: 0 5px 15px 5px !important;";
-const STYLE_HIDE_GRAPHIC_HANDLE_BR = "z-index: auto !important;";
-const STYLE_HIDE_GRAPHIC_AFTER = "display: block !important;";
+const STYLE_BASIC_IFRAME = "border: 1px solid #000; -appkit-box-shadow: inset 1px 1px 0 0 #fff, 5px 5px 0 rgba(0,0,0,.2); box-shadow: inset 1px 1px 0 0 #fff, 5px 5px 0 rgba(0,0,0,.2);";
+const STYLE_LOAD_IFRAME = STYLE_BASIC_IFRAME + " left: 100vw; top: 100vh; width: 101%; height: 101%; pointer-events: none;";
+const STYLE_IFRAME = STYLE_BASIC_IFRAME + " left: auto; top: auto; width: 100%; height: 100%; transform: none;";
+const STYLE_SHOW_INNER_WRAPPER = "padding: 0 5px 15px 5px !important;";
+const STYLE_SHOW_AFTER = "display: block !important;";
+const STYLE_APP_WINDOW_BTN_PART = " no-repeat 50%; background-size: 7px auto;";
+const APP_WINDOW_BTN_MARGIN = "-1px";
+const VIDEO_OVERLAY_ENABLED = true;
+
+// =============================================== CSS CLASSES
+
+document.styleSheets[0].addRule('#' + SIZER_ID, "margin-left: " + APP_WINDOW_BTN_MARGIN + ';');
+document.styleSheets[0].addRule('#' + SIZER_ID + '.' + RESTORE_DOWN_CLASS + ":after", "title: 'Restore Down'; background: url('" + RESTORE_DOWN_ICON + "') " + STYLE_APP_WINDOW_BTN_PART);
+document.styleSheets[0].addRule('#' + SIZER_ID + '.' + MAXIMIZE_CLASS + ":after", "title: 'Maximize'; background: url('" + MAXIMIZE_ICON + "') " + STYLE_APP_WINDOW_BTN_PART);
+document.styleSheets[0].addRule('#' + RELOAD_ID, "margin-left: " + APP_WINDOW_BTN_MARGIN + ';');
+document.styleSheets[0].addRule('#' + RELOAD_ID + ":after", "title: 'Reload';");
+document.styleSheets[0].addRule('#' + DESKTOP_ID + '.' + HIDE_DESKTOP_CLASS + " > div > header", "display: none !important;");
+document.styleSheets[0].addRule('#' + DESKTOP_ID + ":not(." + HIDE_DESKTOP_CLASS + ") #" + APP_ID + HIDE_GRAPHIC_SELECTOR + ' ' + INNER_WRAPPER_SELECTOR, STYLE_SHOW_INNER_WRAPPER);
+document.styleSheets[0].addRule('#' + DESKTOP_ID + ":not(." + HIDE_DESKTOP_CLASS + ") #" + APP_ID + ":after", STYLE_SHOW_AFTER);
 
 // =============================================== VARIABLES
 
+var appIsMaximized = false;
 var loadedYT = false;
 var loadedApp = false;
+var iframeStylesSet = false;
+var videoOverlaySet = false;
 var isMoving = false;
 
 // gets set when loaded
 var youtubeLoader = null;
 var appVideoOverlay = null;
 var appIframe = null;
+var appWindow = null;
+var appInnerContent = null;
+var appInnerWrapper = null;
+var appDragHeader = null;
+var appHandleSizer = null;
+var vueApp = null;
+var desktop = null;
+var taskbar = null;
+var minimizeBtn = null;
+var sizerBtn = null;
+var reloadBtn = null;
+var prevX = null; // in pixels
+var prevY = null; // in pixels
+var prevW = null; // in pixels
+var prevH = null; // in pixels
+
+// =============================================== OBJECTS & RELATED
 
 var minecraft = document.createElement("iframe");
 minecraft.frameborder = "0";
@@ -80,7 +126,7 @@ minecraft.src = APP_WEBSITE;
 minecraft.sandbox = SANDBOX_IFRAME;
 minecraft.style = STYLE_LOAD_IFRAME;
 var minecraftOuterHTML = minecraft.outerHTML;
-var appOuterHTML = minecraftOuterHTML.substring(0, 8) + 'id="' + APP_ID + '" ' + minecraftOuterHTML.substring(8);
+var appOuterHTML = minecraftOuterHTML.substring(0, 8) + 'id="' + IFRAME_ID + '" ' + minecraftOuterHTML.substring(8);
 
 // =============================================== FUNCTIONS
 
@@ -97,40 +143,98 @@ var isHidden = function(el) {
 
 // when the game loads, it needs a first time gui fix
 var fixGuiMC = function(appIframe) {
-    //var iStyle = appWindow.getAttribute("style");
-    //var iPreStyle = iStyle.substring(0, iStyle.indexOf("width"));
-    //appWindow.style = iPreStyle + "width: " + (appWindow.clientWidth - 1) + "px; height: " + (appWindow.clientHeight - 1) + "px;";
-    //setTimeout(function() {appWindow.style = iStyle}, UPDATE_GUI_TIMEOUT2);
     appIframe.style = "pointer-events: all; " + STYLE_IFRAME;
+    document.exitPointerLock();
+    document.getElementsByTagName("section")[0].focus();
 }
 
 // Takes an app (iframe), and replaces the Poolside TV iframe
-var replaceIframe = function(appInnerContent) {
+var replaceIframe = function() {
     // 'reload' iframe by changing the outer HTML (avoids leave site prompt)
     appIframe.outerHTML = appOuterHTML;
-    appIframe = document.getElementById(APP_ID);
+    appIframe = document.getElementById(IFRAME_ID);
     appIframe.focus(); // fixes appGL not loading error on some appsites that start up
 
     // fix Minecraft GUI after every iframe reload
     appIframe.onload = function() {setTimeout(function() {fixGuiMC(appIframe)}, UPDATE_GUI_TIMEOUT)}
 }
 
+// Set the app vue X, Y, W, H
+var storeVueWindowCoords = function() {
+    prevX = vueApp.left;
+    prevY = vueApp.top;
+    prevW = vueApp.width;
+    prevH = vueApp.height;
+}
+
+// Sets the design changes for the app window sizer button
+var setSizerBtnDesign = function(maximizing) {
+    if (maximizing) {
+        storeVueWindowCoords();
+        appWindow.children[1].style.pointerEvents = "none";
+        reloadBtn.style.pointerEvents = "all";
+        minimizeBtn.style.pointerEvents = "all";
+        sizerBtn.style.pointerEvents = "all";
+        sizerBtn.classList.remove(MAXIMIZE_CLASS);
+        sizerBtn.classList.add(RESTORE_DOWN_CLASS);
+        desktop.classList.add(HIDE_DESKTOP_CLASS);
+        window.appWindow = appWindow;
+        vueApp.left = 0;
+        vueApp.top = 0;
+        vueApp.changeWidth(appWindow.parentElement.clientWidth);
+        vueApp.changeHeight(appWindow.parentElement.clientHeight);
+        vueApp.draggable = false;
+    } else {
+        if (prevX == null && prevY == null && prevW == null && prevH == null) storeVueWindowCoords();
+        appWindow.children[1].style.pointerEvents = '';
+        reloadBtn.style.pointerEvents = '';
+        minimizeBtn.style.pointerEvents = '';
+        sizerBtn.style.pointerEvents = '';
+        sizerBtn.classList.add(MAXIMIZE_CLASS);
+        sizerBtn.classList.remove(RESTORE_DOWN_CLASS);
+        desktop.classList.remove(HIDE_DESKTOP_CLASS);
+        vueApp.left = prevX;
+        vueApp.top = prevY;
+        vueApp.changeWidth(prevW);
+        vueApp.changeHeight(prevH);
+        vueApp.draggable = true;
+    }
+}
+
+// Toggles size of window for app
+var toggleSizer = function(choice) {
+    var tempIsMaximized = appIsMaximized;
+    if (exists(choice) && typeof(optional) == "boolean") tempIsMaximized = choice;
+    else tempIsMaximized = !appIsMaximized;
+
+    // only do something when we've changed the setting
+    if (tempIsMaximized != appIsMaximized) {
+        setSizerBtnDesign(tempIsMaximized);
+        appIsMaximized = tempIsMaximized;
+    }
+}
+
 // Converts the Poolside TV into Minecraft
 var convertToMinecraft = function() {
     // MORE VARIABLES
-    var appInnerContent = appIframe.parentElement;
-    var appInnerWrapper = appInnerContent.parentElement.children[0];
-    var appWindow = appInnerWrapper.parentElement.parentElement;
-    var appWindowDragHeader = appWindow.children[1];
-    var appWindowMinIcon = appWindowDragHeader.children[0];
-    var appWindowTitle = appWindowDragHeader.children[2];
+    desktop = document.getElementById(DESKTOP_ID);
+    taskbar = document.querySelector(TASKBAR_SELECTOR);
+    appInnerContent = appIframe.parentElement;
+    appInnerWrapper = appInnerContent.parentElement;
+    appWindow = appInnerWrapper.parentElement;
+    vueApp = appWindow.__vue__;
+    appHandleSizer = appWindow.children[0];
+    appDragHeader = appWindow.children[1];
+    minimizeBtn = appDragHeader.children[0];
+    var appWindowTitle = appDragHeader.children[2];
     var appVideoBar = appInnerContent.children[0];
 
-    // ID the innerContent
+    // ID other variables
+    taskbar.id = TASKBAR_ID;
     appInnerContent.id = APP_SPLASH_ID;
 
     // rename the window
-    appWindow.id = APP_WINDOW_ID;
+    appWindow.id = APP_ID;
     appWindowTitle.innerText = APP_NAME;
 
     // style the window
@@ -140,30 +244,40 @@ var convertToMinecraft = function() {
     splashScreenStyle.backgroundRepeat = "no-repeat";
     splashScreenStyle.backgroundPosition = "center";
     splashScreenStyle.backgroundSize = "contain";
-    document.styleSheets[0].addRule('#' + APP_SPLASH_ID, STYLE_APP_SPLASH);
-    document.styleSheets[0].addRule('#' + APP_WINDOW_ID + HIDE_GRAPHIC_SELECTOR + ' ' + INNER_WRAPPER_SELECTOR, STYLE_HIDE_GRAPHIC_INNER_WRAPPER);
-    document.styleSheets[0].addRule('#' + APP_WINDOW_ID + HIDE_GRAPHIC_SELECTOR + ' ' + HANDLE_BR_SELECTOR, STYLE_HIDE_GRAPHIC_HANDLE_BR);
-    document.styleSheets[0].addRule('#' + APP_WINDOW_ID + HIDE_GRAPHIC_SELECTOR + ":after", STYLE_HIDE_GRAPHIC_AFTER);
+    if (!iframeStylesSet) {
+        iframeStylesSet = true;
+        document.styleSheets[0].addRule('#' + APP_SPLASH_ID, STYLE_APP_SPLASH);
+    }
 
     // replace Poolside TV with the app
-    replaceIframe(appInnerContent);
+    replaceIframe();
 
-    // create a reload button (necessary for when appGL breaks)
-    var reloadIcon = appWindowMinIcon.cloneNode(true);
-    reloadIcon.id = "reload-iframe";
-    reloadIcon.title = "Reload";
-    reloadIcon.setAttribute("style", "margin-left: -1px;");
-    reloadIcon.onclick = function() {replaceIframe(appInnerContent)}
-    appWindowDragHeader.insertBefore(reloadIcon, appWindowMinIcon.nextSibling);
-    document.styleSheets[0].addRule('#' + reloadIcon.id + ":after", "background: url('" + RELOAD_ICON + "') no-repeat 50%; background-size: 7px auto;");
+    // get buttons
+    reloadBtn = minimizeBtn.cloneNode(true);
+    sizerBtn = minimizeBtn.cloneNode(true);
+    minimizeBtn.classList.add(MINIMIZE_CLASS);
+
+    // create additional window buttons
+    // RELOAD (necessary for when appGL breaks)
+    reloadBtn.id = RELOAD_ID;
+    reloadBtn.onmouseup = function() {replaceIframe()}
+    appDragHeader.insertBefore(reloadBtn, minimizeBtn.nextSibling);
+    document.styleSheets[0].addRule('#' + reloadBtn.id + ":after", "background: url('" + RELOAD_ICON + "') " + STYLE_APP_WINDOW_BTN_PART);
+    // SIZER
+    sizerBtn.id = SIZER_ID;
+    sizerBtn.onmouseup = function() {toggleSizer()}
+    appDragHeader.insertBefore(sizerBtn, minimizeBtn.nextSibling);
+    setSizerBtnDesign(appIsMaximized);
 
     // delete the video bar
     appInnerContent.removeChild(appVideoBar);
 
-    // delete the overlay
-    //appInnerContent.removeChild(appVideoOverlay);
-    // or make overlay click through
-    appVideoOverlay.setAttribute("style", "pointer-events: none;");
+    // delete the overlay (if there is one) or make overlay click-through
+    if (!VIDEO_OVERLAY_ENABLED && !videoOverlaySet) {
+        videoOverlaySet = true;
+        if (appVideoOverlay != null) appInnerContent.removeChild(appVideoOverlay);
+        else appVideoOverlay.setAttribute("style", "pointer-events: none;");
+    }
 
     // watch for when resizing to fix issues with it being bugged
     setInterval(function() {
@@ -198,7 +312,7 @@ var waitForShortcut = setInterval(function() {
     }
 }, TENTH_OF_SECOND);
 var waitForIframe = setInterval(function() {
-    appIframe = document.getElementById(APP_ID);
+    appIframe = document.getElementById(IFRAME_ID);
 
     if (exists(appIframe)) clearInterval(waitForIframe);
 }, TENTH_OF_SECOND);
