d1ba6a4fa8bcae2c4f35df79e3c001d0de47ef36
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-06-07 23:34:36 -0700

    Moving button icons for use in other future scripts for the website

--- Poolside FM/Poolside-FM-Minecraft-DLC/Poolside-FM-Minecraft-DLC.user.js
+++ Poolside FM/Poolside-FM-Minecraft-DLC/Poolside-FM-Minecraft-DLC.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Poolside FM - Minecraft DLC
 // @namespace    https://thealiendrew.github.io/
-// @version      0.3.2
+// @version      0.3.3
 // @description  Allows toggling the video to a playable version of Minecraft Classic!
 // @author       AlienDrew
 // @match        https://poolside.fm/*
@@ -52,8 +52,9 @@ const HIDE_DESKTOP_CLASS = "hide-desktop";
 const APP_NAME = "Minecraft";
 const APP_ID = APP_NAME.toLowerCase();
 const APP_WEBSITE = "https://classic.minecraft.net/";
-const APP_RAW_LINK = "https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Poolside%20FM/Poolside-FM-Minecraft-DLC/";
-const APP_ICONS_LINK = APP_RAW_LINK + "icons/";
+const PS_FM_RESOURCES_LINK = "https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Poolside%20FM/";
+const APP_RAW_LINK = PS_FM_RESOURCES_LINK + "Poolside-FM-Minecraft-DLC/";
+const APP_ICONS_LINK = PS_FM_RESOURCES_LINK + "window-button-icons/";
 const APP_FAVICON = APP_RAW_LINK + "favicons/retro_dithered.png";
 const APP_SPLASH = APP_RAW_LINK + "logos/mojang.png";
 const MAXIMIZE_ICON = APP_ICONS_LINK + "maximize.png";
