c77a716902350edcc6cf85e41be3a27e3c109dfc
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-08-30 07:38:24 -0700

    Create BitDay Legacy - Minimal GUI.user.js

---
+++ BitDay/BitDay Legacy - Minimal GUI.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         BitDay (Legacy) - Minimal GUI
+// @namespace    https://thealiendrew.github.io/
+// @version      0.0.1
+// @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/BitDay/BitDay%20Legacy%20-%20Minimal%20GUI.user.js
+// @description  Removes all GUI, except the clock area.
+// @author       AlienDrew
+// @include      /^https?://www\.bitday\.me/legacy*/
+// @grant        none
+// ==/UserScript==
+
+var donateBtn = document.querySelector(".donate");
+var footer = document.querySelector("footer");
+
+donateBtn.style.display = footer.style.display = "none";
