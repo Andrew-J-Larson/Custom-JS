0f92ab35d4792d59c1960972a147144503a81e02
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-09-02 17:27:04 -0700

    Design change
    Remove background semi-transparency in favor for text border (shadows)

--- BitDay/BitDay (Legacy) - Bigger Clock (RPI).user.js
+++ BitDay/BitDay (Legacy) - Bigger Clock (RPI).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BitDay (Legacy) - Bigger Clock (RPI)
 // @namespace    https://thealiendrew.github.io/
-// @version      0.0.1
+// @version      0.0.2
 // @downloadURL  https://github.com/TheAlienDrew/Tampermonkey-Scripts/raw/master/BitDay/BitDay%20(Legacy)%20-%20Bigger%20Clock%20(RPI).user.js
 // @description  Makes the clock much bigger (since zoom glitches position).
 // @author       AlienDrew
@@ -14,4 +14,6 @@ var clockText = clock.firstElementChild;
 
 clock.style.width = "90%";
 clock.style.marginTop = "8.5%";
+clock.style.backgroundColor = "transparent";
 clockText.style.fontSize = "300%";
+clockText.style.textShadow = "-5.5px 0 black, -5.5px 5.5px black, 0 5.5px black, 5.5px 5.5px black, 5.5px 0 black, 5.5px -5.5px black, 0 -5.5px black, -5.5px -5.5px black";
