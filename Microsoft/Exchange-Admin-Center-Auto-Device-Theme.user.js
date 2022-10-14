e766ffbe4b8d46fe2540a56be17ca61bb6d68213
Drew <19640692+TheAlienDrew@users.noreply.github.com>
2022-10-14 09:19:08 -0700

    Remove access to iframes
    Preventative measure, since no iframes are likely to gave the ability to change themes, if anything can be based off of the Teams script.

--- Microsoft/Exchange-Admin-Center-Auto-Device-Theme.user.js
+++ Microsoft/Exchange-Admin-Center-Auto-Device-Theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Exchange admin center - Auto Device Theme
 // @namespace    https://thealiendrew.github.io/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Makes Exchange admin center match the device theme at all times.
 // @author       AlienDrew
 // @license      GPL-3.0-or-later
@@ -10,6 +10,7 @@
 // @downloadURL  https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/Exchange-Admin-Center-Auto-Device-Theme.user.js
 // @icon         https://eacprodeusstorage.blob.core.windows.net/resources/images/favicon_metro.ico
 // @grant        none
+// @noframes
 // ==/UserScript==
 
 /* Copyright (C) 2020  Andrew Larson (thealiendrew@gmail.com)
