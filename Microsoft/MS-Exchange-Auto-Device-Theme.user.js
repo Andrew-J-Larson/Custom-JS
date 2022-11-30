849f0025b368f9378cf07abed07508c36d2b9852
Drew <19640692+TheAlienDrew@users.noreply.github.com>
2022-11-30 13:00:56 -0800

    Deprecating Scripts
    Will need to delete the following scripts in a week from now:
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/Exchange-Admin-Center-Auto-Device-Theme.user.js
      > New script at: https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-Exchange-Auto-Device-Theme.user.js
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-Docs-Auto-Device-Theme.user.js
      > New script at: https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-Learn-Auto-Device-Theme.user.js
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-Office-Auto-Device-Theme.user.js
      > New script at: https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-365-Auto-Device-Theme.user.js

---
+++ Microsoft/MS-Exchange-Auto-Device-Theme.user.js
@@ -0,0 +1,85 @@
+// ==UserScript==
+// @name         Microsoft Exchange - Auto Device Theme
+// @namespace    https://thealiendrew.github.io/
+// @version      1.0.4
+// @description  Makes Microsoft Exchange match the device theme at all times.
+// @author       AlienDrew
+// @license      GPL-3.0-or-later
+// @match        https://admin.exchange.microsoft.com/*
+// @updateURL    https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/MS-Exchange-Auto-Device-Theme.user.js
+// @downloadURL  https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/MS-Exchange-Auto-Device-Theme.user.js
+// @icon         https://eacprodeusstorage.blob.core.windows.net/resources/images/favicon_metro.ico
+// @grant        none
+// @noframes
+// ==/UserScript==
+
+/* Copyright (C) 2020  Andrew Larson (thealiendrew@gmail.com)
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const INTERVAL_SPEED = 100;
+const themeBtnSelector = 'button[data-automation-id="DarkLightBtn"]';
+const themeNotIconSelector = themeBtnSelector + ' > span > i[data-icon-name]';
+
+var watchEventTriggered = false;
+var activeElement = null;
+var themeButton = null;
+var themeNotIcon = null;
+
+function updateTheme(changeToScheme) {
+    themeNotIcon = document.querySelector(themeNotIconSelector);
+    let theme = 'light';
+    if (themeNotIcon.getAttribute('data-icon-name') == 'Light') theme = 'dark';
+
+    if (theme != changeToScheme) {
+        // click the theme switcher
+        themeButton = document.querySelector(themeBtnSelector);
+        themeButton.click();
+
+        if (watchEventTriggered) activeElement.focus();
+    }
+
+    watchEventTriggered = false;
+}
+
+// wait for the page to be fully loaded
+window.addEventListener('load', function () {
+    // need to wait for button & icon to be available
+    let waitingForThemeBtnAndIco = setInterval(function() {
+        themeButton = document.querySelector(themeBtnSelector);
+        themeNotIcon = document.querySelector(themeNotIconSelector);
+
+        if (themeButton && themeNotIcon) {
+            clearInterval(waitingForThemeBtnAndIco);
+
+            // now we can start
+            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+                const newColorScheme = e.matches ? 'dark' : 'light';
+                watchEventTriggered = true;
+                activeElement = document.activeElement;
+                updateTheme(newColorScheme);
+            });
+
+            // first time run
+            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                // dark mode
+                updateTheme('dark');
+            } else {
+                // light mode
+                updateTheme('light');
+            }
+        }
+    }, INTERVAL_SPEED);
+}, false);
