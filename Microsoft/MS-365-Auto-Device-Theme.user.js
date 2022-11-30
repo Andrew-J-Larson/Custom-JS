3b2045578eaefaf23ff6bc548b788b129f787177
Drew <19640692+TheAlienDrew@users.noreply.github.com>
2022-11-30 13:48:09 -0800

    Deprecating Scripts
    Will need to delete the following scripts in a week from now (they have new versions under different file names):
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/Exchange-Admin-Center-Auto-Device-Theme.user.js
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-365-Auto-Device-Theme.user.js
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-Docs-Auto-Device-Theme.user.js
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-Office-Auto-Device-Theme.user.js

--- Microsoft/MS-365-Auto-Device-Theme.user.js
+++ Microsoft/MS-365-Auto-Device-Theme.user.js
@@ -1,147 +1,19 @@
 // ==UserScript==
 // @name         Microsoft 365 Web Apps - Auto Device Theme
 // @namespace    https://thealiendrew.github.io/
-// @version      1.0.4
+// @version      1.0.5
 // @description  Makes any Microsoft 365 web app match the device theme at all times.
 // @author       AlienDrew
 // @license      GPL-3.0-or-later
 // @match        https://*.microsoft365.com/*
 // @match        https://*.office.com/*
 // @match        https://*.office365.com/*
-// @updateURL    https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/MS-365-Auto-Device-Theme.user.js
-// @downloadURL  https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/MS-365-Auto-Device-Theme.user.js
+// @updateURL    https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/MS-365-Web-Apps-Auto-Device-Theme.user.js
+// @downloadURL  https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/MS-365-Web-Apps-Auto-Device-Theme.user.js
 // @icon         https://res-1.cdn.office.net/officehub/images/content/images/favicon-8f211ea639.ico
 // @grant        none
 // @noframes
 // ==/UserScript==
 
-/* Copyright (C) 2020  Andrew Larson (thealiendrew@gmail.com)
+// Please force update this application
 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-const INTERVAL_SPEED = 5; // ms
-const OLD_PAGE_DELAY = 1500; // ms
-
-// needed when screen is small
-const maybeMoreButtonSelector = '#O365_MainLink_Affordance';
-
-// the following if found don't need page to be refreshed
-const settingsButtonSelector = '#O365_MainLink_Settings';
-const firstThemeCardSelector = '#themecardpanel > div > div';
-const themeToggleSwitchSelector = '#DarkModeSwitch';
-
-// the following if found will need the page to be refreshed
-const owaSettingsButtonSelector = '#owaSettingsButton';
-const owaFirstThemeCardSelector = 'div[aria-label="Theme"] > div'
-const owaThemeToggleSwitchSelector = 'button[aria-label="options-quick-darkMode"]';
-
-// the following subdomains have no dark theme selection, so must be excluded
-const excludedSubDomains = ["nam.delve", "tasks", "to-do", "insights.viva", "whiteboard"];
-
-var watchEventTriggered = false;
-var activeElement = null;
-var maybeMoreButton, firstThemeCard, settingsButton; // gets set later
-
-function updateTheme(changeToScheme) {
-    let html = document.querySelector('html');
-
-    let theme = window.__themeState__.theme.black;
-    theme = (theme == "#ffffff" || (theme == "var(--black)" && getComputedStyle(document.documentElement).getPropertyValue('--black') == "#ffffff")) ? 'dark' : 'light';
-
-    if (theme != changeToScheme) {
-        let waitForMoreAndSettings = setInterval(function() {
-            let firstClicked;
-            maybeMoreButton = document.querySelector(maybeMoreButtonSelector);
-            settingsButton = document.querySelector(settingsButtonSelector) || document.querySelector(owaSettingsButtonSelector);
-            if (maybeMoreButton && !settingsButton) {
-                // more button needs to be pressed first
-                maybeMoreButton.click();
-                firstClicked = maybeMoreButton;
-            } else if (settingsButton) {
-                clearInterval(waitForMoreAndSettings);
-
-                // now settings can be opened
-                settingsButton.click();
-                if (!firstClicked) firstClicked = settingsButton;
-
-                let waitForThemeToggle = setInterval(function() {
-                    firstThemeCard = document.querySelector(firstThemeCardSelector) || document.querySelector(owaFirstThemeCardSelector);
-                    let themeToggleSwitch = document.querySelector(themeToggleSwitchSelector) || document.querySelector(owaThemeToggleSwitchSelector);
-                    if (firstThemeCard && themeToggleSwitch) {
-                        clearInterval(waitForThemeToggle);
-
-                        firstThemeCard.click();
-                        themeToggleSwitch.click();
-                        firstClicked.click();
-
-                        // need to wait a short bit for change to go through, only on old pages that need reloading
-                        if (window.userNormalizedTheme) {
-                            setTimeout(function() {
-                                window.location.reload();
-                            }, OLD_PAGE_DELAY);
-                        }
-
-                        if (watchEventTriggered) activeElement.focus();
-                    }
-                }, INTERVAL_SPEED);
-            }
-        }, INTERVAL_SPEED);
-    }
-
-    watchEventTriggered = false;
-}
-
-// wait for the page to be fully loaded
-window.addEventListener('load', function () {
-    let testSubDomainIndex = 0;
-    let testSubDomainEnd = excludedSubDomains.length;
-    let testSubDomainLoop = setInterval(function() {
-        if (window.location.hostname.startsWith(excludedSubDomains[testSubDomainIndex])) {
-            testSubDomainIndex = -1;
-        } else testSubDomainIndex++;
-        // only stop when we've reached that point
-        if (testSubDomainIndex >= testSubDomainEnd) {
-            clearInterval(testSubDomainLoop);
-
-            // will get here if there was no throw issue
-            let waitForThemeAndSettingsAvailable = setInterval(function() {
-                // need to wait for one of the required buttons
-                if (window.__themeState__ && window.__themeState__.theme && window.__themeState__.theme.black && (document.querySelector(maybeMoreButtonSelector) || document.querySelector(settingsButtonSelector) || document.querySelector(owaSettingsButtonSelector))) {
-                    clearInterval(waitForThemeAndSettingsAvailable);
-
-                    // now we can start
-                    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-                        const newColorScheme = e.matches ? 'dark' : 'light';
-                        watchEventTriggered = true;
-                        activeElement = document.activeElement;
-                        updateTheme(newColorScheme);
-                    });
-
-                    // first time run
-                    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                        // dark mode
-                        updateTheme('dark');
-                    } else {
-                        // light mode
-                        updateTheme('light');
-                    }
-                }
-            }, INTERVAL_SPEED);
-        } else if (testSubDomainIndex < 0) {
-            // page has no dark mode, exit
-            clearInterval(testSubDomainLoop);
-        }
-    }, 0);
-}, false);
