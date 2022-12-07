1c676b0e8f9d1e62a34682637fb5f2abdf186ce4
Drew <19640692+TheAlienDrew@users.noreply.github.com>
2022-12-07 09:52:03 -0800

    Realized I never actually changed the URL to the correct one in this
    --- REMINDER
    
    Repo is eventually going to be renamed to `Custom-JS`.
    
    Will need to delete the following scripts on Dec. 7, 2022 (they have new versions under different file names):
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/Exchange-Admin-Center-Auto-Device-Theme.user.js
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-365-Auto-Device-Theme.user.js
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-Docs-Auto-Device-Theme.user.js
     * https://github.com/TheAlienDrew/Tampermonkey-Scripts/blob/master/Microsoft/MS-Office-Auto-Device-Theme.user.js
    
    After the deletion of deprecated user-scripts, need to edit versions and download/update URLs to point to future links (prep for repo name change), then wait another week.
    
    After the deletion of folders, need to rename the repo to `Custom-JS`

--- Microsoft/MS-Learn-Auto-Device-Theme.user.js
+++ Microsoft/MS-Learn-Auto-Device-Theme.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Microsoft Learn - Auto Device Theme
 // @namespace    https://thealiendrew.github.io/
-// @version      1.0.3
+// @version      1.0.4
 // @description  Makes Microsoft Learn match the device theme at all times.
 // @author       AlienDrew
 // @license      GPL-3.0-or-later
-// @match        https://docs.microsoft.com/*
+// @match        https://learn.microsoft.com/*
 // @updateURL    https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/MS-Learn-Auto-Device-Theme.user.js
 // @downloadURL  https://raw.githubusercontent.com/TheAlienDrew/Tampermonkey-Scripts/master/Microsoft/MS-Learn-Auto-Device-Theme.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=learn.microsoft.com
