7222c00ad881e7175c249e072242dc816bb984c4
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2023-06-25 04:23:03 -0700

    Fixed bug with `/link` finally (sorry)

--- !-User-Scripts/Multiplayer Piano/MIDI-Player/MIDI-Player.user.js
+++ !-User-Scripts/Multiplayer Piano/MIDI-Player/MIDI-Player.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multiplayer Piano - MIDI Player
 // @namespace    https://thealiendrew.github.io/
-// @version      3.2.7
+// @version      3.2.8
 // @description  Plays MIDI files!
 // @author       AlienDrew
 // @license      GPL-3.0-or-later
@@ -63,7 +63,7 @@ const VERSION = SCRIPT.version;
 const DESCRIPTION = SCRIPT.description;
 const AUTHOR = SCRIPT.author;
 const DOWNLOAD_URL = SCRIPT.downloadURL;
-const SOURCE_URL = GM_getResourceURL("UserScriptSource");
+const SOURCE_URL = GM_getResourceText("UserScriptSource");
 
 // =============================================== FILES
 
--- !-User-Scripts/Multiplayer Piano/MIDI-Player/Minecraft-Music/Auto-Player.user.js
+++ !-User-Scripts/Multiplayer Piano/MIDI-Player/Minecraft-Music/Auto-Player.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multiplayer Piano - Minecraft Music Auto Player
 // @namespace    https://thealiendrew.github.io/
-// @version      3.2.7
+// @version      3.2.8
 // @description  Plays Minecraft music!
 // @author       AlienDrew
 // @license      GPL-3.0-or-later
@@ -66,7 +66,7 @@ const VERSION = SCRIPT.version;
 const DESCRIPTION = SCRIPT.description;
 const AUTHOR = SCRIPT.author;
 const DOWNLOAD_URL = SCRIPT.downloadURL;
-const SOURCE_URL = GM_getResourceURL("UserScriptSource");
+const SOURCE_URL = GM_getResourceText("UserScriptSource");
 
 // =============================================== FILES
 
