66c8daa73e5c3dfe02b648d221d8e18d8287b7dc
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2023-07-02 14:15:19 -0700

    Now should work with sustain midi events
    + fixed velocity calculation

--- Any/Multiplayer Piano/MIDI-Player/MIDI-Player.js
+++ Any/Multiplayer Piano/MIDI-Player/MIDI-Player.js
@@ -1,7 +1,7 @@
 // ==JavaScript==
 const NAME = "Multiplayer Piano - MIDI Player";
 const NAMESPACE = "https://thealiendrew.github.io/";
-const VERSION = "3.7.1";
+const VERSION = "3.7.2";
 const DESCRIPTION = "Plays MIDI files!";
 const AUTHOR = "AlienDrew";
 const LICENSE = "GPL-3.0-or-later";
@@ -58,7 +58,7 @@ if (!validWebsite) {
     throw "Not on the Multiplayer Piano website, or compatible clone.";
 }
 
-// should be global: MPP, MidiPlayer
+// should be global: $, MPP, EventEmitter, mixin, MidiPlayer */
 
 // =============================================== SCRIPT CONSTANTS
 
@@ -1426,23 +1426,33 @@ Player.on('playing', function(currentTick) {
 Player.on('midiEvent', function(event) {
     // Do something when a MIDI event is fired.
     // (this is the same as passing a function to MidiPlayer.Player() when instantiating.
+
+    // disable percussion instrument channels
     if (!percussionOption &&
          (event.channel == PERCUSSION_CHANNELS[0] || event.channel == PERCUSSION_CHANNELS[1])) return;
+
+    // check event for note on/off and controller changes (sustain)
     let currentEvent = event.name;
-/* MAY NOT NEED */ // if (!exists(currentEvent) || currentEvent == "") return;
-if (!currentEvent || currentEvent == '') console.log('FOUND NO NAME EVENT');
-//console.log(event.noteName);
-    let currentNote = (currentEvent.indexOf("Note") == 0) ? (
-/* MAY NOT NEED */ //exists(event.noteName) ? mppPianoNotes[event.noteName] : null
-        mppPianoNotes[event.noteName]
-    ) : null;
+    let currentNote = exists(event.noteNumber) ? mppPianoNotes[event.noteNumber - 21] : null;
+    // Note on
     if (currentEvent == "Note on") {
-        let mppNoteVelocity = (event.velocity ? event.velocity/100 : 0);
+        let mppNoteVelocity = (event.velocity ? event.velocity/127 : 0);
         MPP.press(currentNote, mppNoteVelocity);
-        if (event.velocity == 0 || !sustainOption) MPP.release(currentNote);
+        if (!sustainOption || event.velocity == 0) MPP.release(currentNote);
     }
+    // Note off
     if (sustainOption && (currentEvent == "Note off")) {
         MPP.release(currentNote);
+    }
+    // Controller Change
+    if (currentEvent == "Controller Change") {
+        if (event.noteNumber == 64) {
+            if (event.velocity > 20) {
+                MPP.pressSustain();
+            } else {
+                MPP.releaseSustain();
+            }
+        }
     }
 });
 
@@ -1677,13 +1687,12 @@ let waitForMPP = setInterval(function() {
             mppChatSend(PRE_MSG + " This version of Multiplayer Piano doesn't support notifications, please check console for the notification.");
         }
         // setup midi player needs
-        mppPianoNotes = {A0:"a-1",Bb0:"as-1",B0:"b-1",C1:"c0",Db1:"cs0",D1:"d0",Eb1:"ds0",E1:"e0",F1:"f0",Gb1:"fs0",G1:"g0",Ab1:"gs0",A1:"a0",Bb1:"as0",B1:"b0",C2:"c1",Db2:"cs1",D2:"d1",Eb2:"ds1",E2:"e1",F2:"f1",Gb2:"fs1",G2:"g1",Ab2:"gs1",A2:"a1",Bb2:"as1",B2:"b1",C3:"c2",Db3:"cs2",D3:"d2",Eb3:"ds2",E3:"e2",F3:"f2",Gb3:"fs2",G3:"g2",Ab3:"gs2",A3:"a2",Bb3:"as2",B3:"b2",C4:"c3",Db4:"cs3",D4:"d3",Eb4:"ds3",E4:"e3",F4:"f3",Gb4:"fs3",G4:"g3",Ab4:"gs3",A4:"a3",Bb4:"as3",B4:"b3",C5:"c4",Db5:"cs4",D5:"d4",Eb5:"ds4",E5:"e4",F5:"f4",Gb5:"fs4",G5:"g4",Ab5:"gs4",A5:"a4",Bb5:"as4",B5:"b4",C6:"c5",Db6:"cs5",D6:"d5",Eb6:"ds5",E6:"e5",F6:"f5",Gb6:"fs5",G6:"g5",Ab6:"gs5",A6:"a5",Bb6:"as5",B6:"b5",C7:"c6",Db7:"cs6",D7:"d6",Eb7:"ds6",E7:"e6",F7:"f6",Gb7:"fs6",G7:"g6",Ab7:"gs6",A7:"a6",Bb7:"as6",B7:"b6",C8:"c7"};
-        //mppPianoNotes = Object.keys(MPP.piano.keys);
+        mppPianoNotes = Object.keys(MPP.piano.keys);
         // won't work right if press/release sustain keys aren't available
         let compatitbilityError = '';
         if (!exists(MPP.pressSustain) && !exists(MPP.releaseSustain)) {
-            compatitbilityError = "Looks like this version of Multiplayer Piano is incompatible with this mod.<br>" + 
-                                  "Things likely won't work as expected!<br>" + 
+            compatitbilityError = "Looks like this version of Multiplayer Piano is incompatible with this mod.<br>" +
+                                  "Things likely won't work as expected!<br>" +
                                   "Ask the website owner if they can update their version of Muliplayer Piano.<br><br>";
         }
         // create any buttons or other web page elements for mod
